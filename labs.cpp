695337a0f342631a4cd6dfbb7e1ccbd4bacd91ad
Sergey <90379389+SergeyKlimenkov221-331@users.noreply.github.com>
2022-09-21 20:53:20 +0300

    Update labs.cpp

--- labs.cpp
+++ labs.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <iomanip>
 #include <bitset>
@@ -7,58 +7,43 @@ using namespace std;
 
 int main() 
 {
-    
     setlocale(LC_ALL, "Rus");
+
+    cout << "Задание№1" << endl;
     cout << "Клименков Сергей 221-331 " << endl;
+    
+    cout << "Задание№2" << endl;
+    cout << "минимальные максимальные значения встроенных типов данных и их размер" << endl;
+    cout << "min int=" << sizeof(int) << ",max int=" << fixed << setprecision(2) << (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl;
+    cout << "min unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0) - 1) << endl;
+    cout << "min short=" << sizeof(short) << ",max short=" << fixed << setprecision(2) << (pow(2, sizeof(short) * 8.0 - 1) - 1) << endl;
+    cout << "min unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
+    cout << "min long=" << sizeof(long int) << ",max long=" << fixed << setprecision(2) << (pow(2, sizeof(long int) * 8.0 - 1) - 1) << endl;
+    cout << "min long long=" << sizeof(unsigned long int) << ",max long long=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned long int) * 8.0) - 1) << endl;
+    cout << "min double=" << sizeof(double) << ",max double=" << fixed << setprecision(2) << (pow(2, sizeof(double) * 8.0 - 1) - 1) << endl;
+    cout << "min char=" << sizeof(char) << ",max char=" << fixed << setprecision(2) << (pow(2, sizeof(char) * 8.0) - 1) << endl;
+    cout << "min bool=" << sizeof(bool) << ",max bool=" << fixed << setprecision(2) << (pow(2, sizeof(bool) * 8.0) - 1) << endl;
 
+    cout << "Задание№3" << endl;
+    int i;
+    cout << "Введите число:";
+    cin >> i;
+    cout << "В шестнадцатиричном виде:" << std::hex << i << std::endl;
+    cout << "В бинарном виде:" << std::bitset<16>(i) << std::endl;
+
+    cout << "Задание№4" << endl;
+    int a, b;
+    cout << "Впишите переменные: ";
+    cin >> a >> b;
+    cout << "x = " << b << "/" << a << " OR " << b / a << endl;
+
+    cout << "Задание№5" << endl;
     int x0, y0, x1, y1;
-
     cout << "Первая координата: ";
     cin >> x0 >> y0;
     cout << "Вторая координата: ";
     cin >> x1 >> y1;
     cout << "Середина отрезка = " << (x0 + x1) / 2 << " ; " << (y0 + y1) / 2 << endl;
 
-    int a, b;
-    cout << "Впишите переменные: ";
-    cin >> a >> b;
-    cout << "x = " << b << "/" << a << " OR " << b / a << endl;
-
-    int i;
-    cout << "Введите число:";
-    cin >> i;
-
-    cout << "В шестнадцатиричном виде:" << std::hex << i << std::endl;
-
-    cout << "В бинарном виде:" << std::bitset<16>(i) << std::endl;
-
-    cout << "минимальные максимальные значения встроенных типов данных и их размер" << endl;
-
-
-    cout << "     data type      " << "byte" << "      " << "    max  " << endl
-        << "bool               =  " << sizeof(bool) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(bool) * 8.0) - 1) << endl
-        << "char               =  " << sizeof(char) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(char) * 8.0) - 1) << endl
-        << "short int          =  " << sizeof(short int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(short int) * 8.0 - 1) - 1) << endl
-        << "unsigned short int =  " << sizeof(unsigned short int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(unsigned short int) * 8.0) - 1) << endl
-        << "int                =  " << sizeof(int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl
-        << "unsigned int       =  " << sizeof(unsigned int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(unsigned int) * 8.0) - 1) << endl
-        << "long int           =  " << sizeof(long int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(long int) * 8.0 - 1) - 1) << endl
-        << "unsigned long int  =  " << sizeof(unsigned long int) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(unsigned long int) * 8.0) - 1) << endl
-        << "float              =  " << sizeof(float) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(float) * 8.0 - 1) - 1) << endl
-        << "double             =  " << sizeof(double) << "         " << fixed << setprecision(2)
-        << (pow(2, sizeof(double) * 8.0 - 1) - 1) << endl;
-    system("pause");
-
-
     return 0;
-
 }
