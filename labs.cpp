64fce01ed78fbf75d587b5c8a8c3b51b66f6c69c
Sergey <90379389+SergeyKlimenkov221-331@users.noreply.github.com>
2022-09-21 21:04:15 +0300

    Update labs.cpp

--- labs.cpp
+++ labs.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 int main() 
 {
-    setlocale(LC_ALL, "Rus");
+    setlocale(LC_ALL, "Rus");  // Позволяет писать на русском
 
-    cout << "Задание№1" << endl;
+    cout << "Задание№1" << endl; //Перевод на следующую строку
     cout << "Клименков Сергей 221-331 " << endl;
     
     cout << "Задание№2" << endl;
-    cout << "минимальные максимальные значения встроенных типов данных и их размер" << endl;
-    cout << "min int=" << sizeof(int) << ",max int=" << fixed << setprecision(2) << (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl;
+    cout << "минимальные/максимальные значения встроенных типов данных и их размер" << endl;
+    cout << "min int=" << sizeof(int) << ",max int=" << fixed<< setprecision(2)<< (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl;
     cout << "min unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0) - 1) << endl;
     cout << "min short=" << sizeof(short) << ",max short=" << fixed << setprecision(2) << (pow(2, sizeof(short) * 8.0 - 1) - 1) << endl;
     cout << "min unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
@@ -46,4 +46,5 @@ int main()
     cout << "Середина отрезка = " << (x0 + x1) / 2 << " ; " << (y0 + y1) / 2 << endl;
 
     return 0;
+//sizeof-Определяет размер типа данных; fixed-Представляет числа с плавающей запятой; setprecision-установкa чисел с плавающей запятой; pow-вычисляет Х в степени У. 
 }
