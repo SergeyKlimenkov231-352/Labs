07cfa17b03060c365a1ce398226fe21ddaff6a3c
Sergey <90379389+SergeyKlimenkov221-331@users.noreply.github.com>
2022-09-22 09:07:31 +0300

    Update laba1.cpp

--- laba1.cpp
+++ laba1.cpp
@@ -14,15 +14,15 @@ int main()
     
     cout << "Задание№2" << endl;
     cout << "минимальные/максимальные значения встроенных типов данных и их размер" << endl;
-    cout << "min int=" << sizeof(int) << ",max int=" << fixed<< setprecision(2)<< (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl;
-    cout << "min unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0) - 1) << endl;
-    cout << "min short=" << sizeof(short) << ",max short=" << fixed << setprecision(2) << (pow(2, sizeof(short) * 8.0 - 1) - 1) << endl;
-    cout << "min unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
-    cout << "min long=" << sizeof(long int) << ",max long=" << fixed << setprecision(2) << (pow(2, sizeof(long int) * 8.0 - 1) - 1) << endl;
-    cout << "min long long=" << sizeof(unsigned long int) << ",max long long=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned long int) * 8.0) - 1) << endl;
-    cout << "min double=" << sizeof(double) << ",max double=" << fixed << setprecision(2) << (pow(2, sizeof(double) * 8.0 - 1) - 1) << endl;
-    cout << "min char=" << sizeof(char) << ",max char=" << fixed << setprecision(2) << (pow(2, sizeof(char) * 8.0) - 1) << endl;
-    cout << "min bool=" << sizeof(bool) << ",max bool=" << fixed << setprecision(2) << (pow(2, sizeof(bool) * 8.0) - 1) << endl;
+    cout << "size int=" << sizeof(int) << ",max int=" << fixed<< setprecision(2)<< (pow(2, sizeof(int) * 8.0 - 1) - 1) << endl;
+    cout << "size unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0-1) - 1) << endl;
+    cout << "size short=" << sizeof(short) << ",max short=" << fixed << setprecision(2) << (pow(2, sizeof(short) * 8.0 - 1) - 1) << endl;
+    cout << "size unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
+    cout << "size long=" << sizeof(long int) << ",max long=" << fixed << setprecision(2) << (pow(2, sizeof(long int) * 8.0 - 1) - 1) << endl;
+    cout << "size long long=" << sizeof(unsigned long int) << ",max long long=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned long int) * 8.0) - 1) << endl;
+    cout << "size double=" << sizeof(double) << ",max double=" << fixed << setprecision(2) << (pow(2, sizeof(double) * 8.0 - 1) - 1) << endl;
+    cout << "size char=" << sizeof(char) << ",max char=" << fixed << setprecision(2) << (pow(2, sizeof(char) * 8.0) - 1) << endl;
+    cout << "size bool=" << sizeof(bool) << ",max bool=" << fixed << setprecision(2) << (pow(2, sizeof(bool) * 8.0) - 1) << endl;
 
     cout << "Задание№3" << endl;
     int i;
