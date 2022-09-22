6a73ed3b97e02d687f61c091cc9df7d54ce9ad85
Sergey <90379389+SergeyKlimenkov221-331@users.noreply.github.com>
2022-09-22 17:24:11 +0300

    Update laba1.cpp

--- laba1.cpp
+++ laba1.cpp
@@ -5,23 +5,23 @@
 #include <clocale>
 using namespace std;
 
-int main() 
+int main()
 {
     setlocale(LC_ALL, "Rus");  // Позволяет писать на русском
 
     cout << "Задание№1" << endl; //Перевод на следующую строку
     cout << "Клименков Сергей 221-331 " << endl;
-    
+
     cout << "Задание№2" << endl;
     cout << "минимальные/максимальные значения встроенных типов данных и их размер" << endl;
-    cout << "size int=" << sizeof(int) << ",max int=" <<INT_MAX<<",min int="<<INT_MIN<< endl;
-    cout << "size unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0-1) - 1) << endl;
-    cout << "size short=" << sizeof(short) << ",max short=" << fixed << setprecision(2) << (pow(2, sizeof(short) * 8.0 - 1) - 1) << endl;
-    cout << "size unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
-    cout << "size long=" << sizeof(long int) << ",max long=" << fixed << setprecision(2) << (pow(2, sizeof(long int) * 8.0 - 1) - 1) << endl;
+    cout << "size int=" << sizeof(int) << ",max int=" << INT_MAX << ",min int=" << INT_MIN << endl;
+    cout << "size unsigned int=" << sizeof(unsigned int) << ",max unsigned int=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned int) * 8.0 - 1) - 1) << endl;
+    cout << "size short=" << sizeof(short) << ",max short=" <<SHRT_MAX<<",min short="<<SHRT_MIN << endl;
+    cout << "size unsigned short=" << sizeof(unsigned short) << ",max unsigned short=" <<fixed << setprecision(2) << (pow(2, sizeof(unsigned short) * 8.0) - 1) << endl;
+    cout << "size long=" << sizeof(long int) << ",max long=" << LONG_MAX<<".min long"<<LONG_MIN<< endl;
     cout << "size long long=" << sizeof(unsigned long int) << ",max long long=" << fixed << setprecision(2) << (pow(2, sizeof(unsigned long int) * 8.0) - 1) << endl;
-    cout << "size double=" << sizeof(double) << ",max double=" << fixed << setprecision(2) << (pow(2, sizeof(double) * 8.0 - 1) - 1) << endl;
-    cout << "size char=" << sizeof(char) << ",max char=" << CHAR_MAX<<",min char="<<CHAR_MIN<< endl;
+    cout << "size double=" << sizeof(double) << ",max double=" << DBL_MAX << ",min double" << DBL_MIN << endl;
+    cout <<"size char="<< sizeof(char)<<",max char="<<CHAR_MAX << ",min char=" << CHAR_MIN << endl;
     cout << "size bool=" << sizeof(bool) << ",max bool=" << fixed << setprecision(2) << (pow(2, sizeof(bool) * 8.0) - 1) << endl;
 
     cout << "Задание№3" << endl;
@@ -38,7 +38,7 @@ int main()
     int a, b;
     cout << "Впишите переменные: ";
     cin >> a >> b;
-    cout << "x = " <<(float) b << "/" <<(float) a << " OR " <<(float) b /(float) a << endl;
+    cout << "x = " << (float)b << "/" << (float)a << " OR " << (float)b / (float)a << endl;
 
     cout << "Задание№5" << endl;
     int x0, y0, x1, y1;
@@ -46,8 +46,8 @@ int main()
     cin >> x0 >> y0;
     cout << "Вторая координата: ";
     cin >> x1 >> y1;
-    cout << "Середина отрезка = " << ((float)x0 +(float)x1) / 2 << " ; " << ((float)y0 +(float)y1) / 2 << endl;
+    cout << "Середина отрезка = " << ((float)x0 + (float)x1) / 2 << " ; " << ((float)y0 + (float)y1) / 2 << endl;
 
     return 0;
-//sizeof-Определяет размер типа данных; fixed-Представляет числа с плавающей запятой; setprecision-установкa чисел с плавающей запятой; pow-вычисляет Х в степени У. 
+    //sizeof-Определяет размер типа данных;
 }
